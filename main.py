a7ecabbebb9596f218b6a340086a7320d910e191
Megan <111366835+MeganKennerley@users.noreply.github.com>
2023-06-21 14:42:26 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,58 @@
+from turtle import Screen
+from paddle import Paddle
+from ball import Ball
+from scoreboard import Scoreboard
+import time
+
+screen = Screen()
+screen.bgcolor("black")
+screen.setup(width=800, height=600)
+screen.title("Pong")
+screen.tracer(0)
+
+l_paddle = Paddle((-350, 0))
+r_paddle = Paddle((350, 0))
+
+ball = Ball()
+score = Scoreboard()
+
+screen.listen()
+screen.onkey(r_paddle.go_up, "Up")
+screen.onkey(r_paddle.go_down, "Down")
+screen.onkey(l_paddle.go_up, "w")
+screen.onkey(l_paddle.go_down, "s")
+
+
+game_on = True
+while game_on:
+    time.sleep(ball.move_speed)
+    screen.update()
+    ball.move()
+
+    # detect collision with top and bottom wall
+    if ball.ycor() > 280 or ball.ycor() < -270:
+        ball.bounce_y()
+
+    # detect collision with right paddle
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
+    """ My attempt """
+    # elif ball.distance(r_paddle) > 50 and ball.xcor() > 380:
+    #     ball.home()
+    #     ball.bounce_x()
+    # elif ball.distance(l_paddle) > 50 and ball.xcor() < -390:
+    #     ball.home()
+    #     ball.bounce_x()
+
+    # detect if r paddle misses
+    if ball.xcor() > 380:
+        ball.reset_position()
+        score.l_point()
+
+    # detect if l paddle misses
+    if ball.xcor() < -390:
+        ball.reset_position()
+        score.r_point()
+
+
+screen.exitonclick()
